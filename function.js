971ed3338b02c832d3951d25b7c0fa19c1c408f0
Reeba <167805857+ReebaDevCraft@users.noreply.github.com>
2025-02-24 22:25:24 +0500

    Update function.js

--- function.js
+++ function.js
@@ -2,15 +2,13 @@ function reverseString(str) {
     return str.split('').reverse().join('');
 }
 
-// Example usage
-console.log(reverseString("Hello, Reeba!")); // Output: "!abeeR ,olleH"
+console.log(reverseString("Hello, Reeba!")); 
 
 function removeDuplicates(arr) {
     return [...new Set(arr)];
 }
 
-// Example usage
-console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Output: [1, 2, 3, 4, 5]
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); 
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
