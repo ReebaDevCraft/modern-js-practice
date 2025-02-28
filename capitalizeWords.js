92a25be71a7951e92e93808e47c06c18bd451c41
Reeba <167805857+ReebaDevCraft@users.noreply.github.com>
2025-02-28 22:26:41 +0500

    Update capitalizeWords.js
    Capitalize the First Letter of Each Word in a String

--- capitalizeWords.js
+++ capitalizeWords.js
@@ -1,1 +1,3 @@
+const capitalizeWords = (str) => str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+console.log(capitalizeWords("hello world from es6")); // Output: Hello World From Es6
 
