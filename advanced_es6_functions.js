f865c7e38fe2e588d79d9ad06ff586ebd68e9039
Reeba <167805857+ReebaDevCraft@users.noreply.github.com>
2025-02-28 22:14:44 +0500

    Update advanced_es6_functions.js
    Uses the reduce() method to find the maximum number in an array.

--- advanced_es6_functions.js
+++ advanced_es6_functions.js
@@ -1,3 +1,3 @@
 const findMax = (arr) => arr.reduce((max, num) => (num > max ? num : max), arr[0]);
-console.log(findMax([10, 25, 6, 78, 3])); // Output: 78
+console.log(findMax([10, 25, 6, 78, 3])); 
 
